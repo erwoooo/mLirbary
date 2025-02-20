12e666214b77981d47425a190cb779f94a151a3e
erwoooo <760941284@qq.com>
2025-02-20 18:24:55 +0800

    发布插件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.load.kotlin.signatures
+
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -8,11 +12,11 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.lucky.library"
+//        applicationId = "com.lucky.library"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+//        versionCode = 1
+//        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -32,6 +36,64 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+}
+
+
+afterEvaluate {
+    publishing{
+        publications{
+            create<MavenPublication>("aar"){
+                groupId = "com.lucky.library"
+                artifactId = "mylibrary"
+                version = "1.0.0"
+
+                //包含aar
+                from(components["release"])
+
+                pom {
+                    name.set("mylibrary")
+                    description.set("test my library")
+                    url.set("https://github.com/erwoooo/mLirbary")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("lucky")
+                            name.set("erwoooo")
+                            email.set("653429798@qq.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://github.com/erwoooo/mLirbary.git")
+
+                        developerConnection.set("scm:git:ssh://github.com/erwoooo/mLirbary.git")
+                        url.set("https://github.com/erwoooo/mLirbary")
+                    }
+                }
+            }
+        }
+
+        repositories{
+            maven {
+                name="GitHubPackages"
+                url = uri("https://github.com/erwoooo/mLirbary")
+                credentials {
+                    username= (project.findProject("gpr.user")?:System.getenv("GPR_USER")) as? String
+                    password = (project.findProject("gpr.key")?:System.getenv("GPR_KEY")) as? String
+                }
+            }
+        }
+    }
+
+    signing{
+        useInMemoryPgpKeys(System.getenv("SINGING_KEY"),System.getenv("SINGING_PASSWORD"))
+        sign(publishing.publications["aar"])
     }
 }
 
