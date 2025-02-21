eeb936593027422bb6454e3a70e8e5c4103a3611
erwoooo <760941284@qq.com>
2025-02-21 08:57:16 +0800

    发布插件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
 //        applicationId = "com.lucky.library"
         minSdk = 24
-        targetSdk = 33
+//        targetSdk = 33
 //        versionCode = 1
 //        versionName = "1.0"
 
---
+++ app/src/main/java/com/lucky/library/Lucky.kt
@@ -0,0 +1,11 @@
+package com.lucky.library
+
+/**
+ * @Author cw
+ * @Used
+ * @Date 2025/2/20 18:31
+ */
+object Lucky {
+
+    const val M_NAME = "Lucky"
+}
